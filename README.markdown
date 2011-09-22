cf767952aba8756d5ed8a025bb1c0152851ef09d
Micah Cooper <mrmicahcooper@gmail.com>
2011-09-22 18:19:39 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+#Buildy Build
+
+- Create your own website with nifty features:
+-- Create all the pages you want
+-- Pages can be normal pages or blogs
+-- Add users if you want
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buildybuild::Application.config.secret_token = '31d25e0b89cf4c4125e453950d5763494177019bc4af96872f56289a4f66f5d9b7331b7f40f98ed4fb6b4b8188e5d0c198ed2d1646fc76bf5f90cfa028651706'
