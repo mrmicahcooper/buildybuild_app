56383e938cee66be820b64a98ac016e7c123daca
Micah Cooper <mrmicahcooper@gmail.com>
2011-09-30 23:32:40 -0400

    Deleted website_settings duplicate migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110927035550) do
+ActiveRecord::Schema.define(:version => 20110927040942) do
 
   create_table "pages", :force => true do |t|
     t.string   "name"
